68ab7afff608390d3b4ed0b9a4119ac50dbcdf24
natanael123soares <105888082+natanael123soares@users.noreply.github.com>
2022-07-31 22:35:41 -0300

    Responsividade e ajustes layout

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    // document.addEventListener('resize', function ReSize() {
+    //     var width = window.innerWidth
+    //     var text =document.getElementById('h1')
+    //     if (width.value = 1440) {
+    //         text.innerHTML =  <h1> Make <br/>  remote work</h1> 
+    //     }
+    // })
+    if (window.matchMedia("(max-width: 768px)").matches) {
+       alert('funcionou')
+    }
