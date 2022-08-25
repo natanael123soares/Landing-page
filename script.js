2739e7e819d0ca5169182841cdebee2d20179408
natanael123soares <105888082+natanael123soares@users.noreply.github.com>
2022-08-25 00:10:56 -0300

    trasiçao menu burguer

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ var image1 =document.querySelector('.img')
 if (width <= 768)
   image1.src = "../images/image-hero-mobile.png";
 
+function toggleMenu(){
+  var nav = document.querySelector('#nav')
+  nav.classList.toggle('active')
+}
